4dbbb939717f33683912af0749a1e791579332dd
Xiaojin Li <shorjin09@gmail.com>
2023-09-03 19:10:42 -0400

    add new feature-pagniation

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import React from 'react';
-import {BrowserRouter,Route, Routes} from "react-router-dom";
-import Home from "./components/home";
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './components/home';
 import Navbar from './components/navbar';
 import Form from './components/Form';
-import SearchImage from './components/search'
+import SearchImage from './components/search';
 import Footer from './components/footer';
-import Geo from './components/geo'
+import Geo from './components/geo';
 
-
-
-const App =function(){
-    return(
-
-        <div className="App">
-            <BrowserRouter>
-              
-
-                <Routes>
-                        <Route path='/' element={<Navbar/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path="search" element={<SearchImage/>}   />
-                        <Route path="form" element={<Form/>}   />
-                        <Route path="geo" element={<Geo/>}   />
-                        
-                
-                        </Route> 
-                    
-                         
-                </Routes>
-
-
-                <Footer />
-             
-             
-                
-                
-            
-
-            </BrowserRouter>
-            
-            </div>
-    )
-}
+const App = function () {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<SearchImage />} />
+          <Route path="form" element={<Form />} />
+          <Route path="geo" element={<Geo />} />
+          {/* Use Navigate to redirect to Home on unknown routes */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
+
